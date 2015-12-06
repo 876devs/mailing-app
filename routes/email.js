ee6e4490ee97afafeccae92d0bbeb0b22397ba29
tremainebuchanan <tremainekbuchanan@gmail.com>
2015-12-05 19:08:44 -0500

    Retrieving client info from db

--- routes/email.js
+++ routes/email.js
@@ -10,18 +10,24 @@ var Client = require('../models/client');
  * Creates a client from the request body.
  */
 router.post('/email', function(req, res){
-    var from = process.env.REPLY_TO || "do_not_reply@gmail.com";
-    var email = new sendgrid.Email({
-        to: 'tremainekbuchanan@gmail.com',
-        from: from,
-        subject: 'Testing Mailing Feature',
-        text: 'Hello World'
-    });
+    var message = '';
+    Client.find().exec(function(err, client){
+        if(err) {
+            message = 'Error';
+        }else{
+            var from = process.env.REPLY_TO || "876devs@gmail.com",
+                email = new sendgrid.Email({
+                    to: client.email,
+                    from: from,
+                    subject: 'Testing Mailing Feature',
+                    text: 'Hello World' + ' ' + client.name
+                });
 
-    sendgrid.send(email, function(err, json){
-       if(err) res.json({message: 'Email not sent'});
-        console.log(json);
-        res.json({message: "Sent successfully"});
+            sendgrid.send(email, function(err, json){
+                if(err) message = 'Error, Send Grid';
+            });
+        }
+        res.json('Message Sent');
     });
 });
 

11731cff237c42c9a280edc2cb03f5bd4c0e5544
tremainebuchanan <tremainekbuchanan@gmail.com>
2015-12-05 19:48:12 -0500

    Added async feature for sending email

--- routes/email.js
+++ routes/email.js
@@ -5,30 +5,31 @@ var express = require('express');
 var router = express.Router();
 var sendgrid = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
 var Client = require('../models/client');
+var async_ = require('async');
 
 /**
  * Creates a client from the request body.
  */
 router.post('/email', function(req, res){
-    var message = '';
-    Client.find().exec(function(err, client){
-        if(err) {
-            message = 'Error';
-        }else{
-            var from = process.env.REPLY_TO || "876devs@gmail.com",
-                email = new sendgrid.Email({
-                    to: client.email,
-                    from: from,
-                    subject: 'Testing Mailing Feature',
-                    text: 'Hello World' + ' ' + client.name
-                });
-
+    var email = new sendgrid.Email(),
+        message = "";
+    Client.find().exec(function(err, clients){
+       if(err) message = "Error Occurred Client";
+        async_.each(clients, function(client, callback){
+            email.addTo(client.email);
+            email.setFrom(process.env.REPLY_TO || "876devs@gmail.com");
+            email.setText('Testing 123');
+            email.setSubject('From Mailing App');
             sendgrid.send(email, function(err, json){
-                if(err) message = 'Error, Send Grid';
-            });
-        }
-        res.json('Message Sent');
+                if(err) message = "Error Send Grid";
+                callback(message);
+            })
+        }, function(err,message){
+            message = "All went well";
+            res.json({message: message});
+        });
     });
+
 });
 
 module.exports = router;
